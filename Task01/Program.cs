3e9b8035ef73947de58608f76b9350ad311838e9
Akif <Akif@KOMPUTER>
2023-04-06 14:50:22 +0200

    Добавьте файлы проекта.

---
+++ Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int A = Int32.Parse(Console.ReadLine());
+int B = Int32.Parse(Console.ReadLine());
+int C = Int32.Parse(Console.ReadLine());
+if (C > A + B)
+{
+    Console.WriteLine("3начение С превышает размер сторон прямоугольника, введите заного данные.");
+    return;
+}
+int squadAB = A * B;
+Console.WriteLine("Прямоугольник размера A*B равен " + squadAB);
+int squadC = C * C;
+Console.WriteLine("Квадрат стороны C равен " + squadC);
+int squadCount = squadAB / squadC;
+Console.WriteLine("На прямоугольнике размера A*B размещено максимально " + squadCount + " квадрат со стороной C");
+
+int noUseSquad = squadAB % squadC;
+
+Console.WriteLine("Площадь незанятой части прямоугольника равен " + noUseSquad);
+
+
